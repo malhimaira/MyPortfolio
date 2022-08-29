d6c52f767b437578ab309d482830a81a7020977d
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-08-28 23:44:07 -0400

    redid some tiles styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,27 +3,29 @@ import { Paper, Typography, Grid, Box } from '@mui/material';
 function Home() {
   return (
     <div>
-      <Box sx={{ flexGrow: 1, padding: 1 }} textAlign='left'>
-        <Grid container spacing={2} columns={12}>
-          <Grid item xs={12} sm={6} md={6} >
-            <Paper elevation={0}>
-              <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}  >
-            <Paper>
-              <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}  >
-            <Paper>
-              <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}  >
-            <Paper>
-              <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
+      <Box sx={{ flexGrow: 1, padding: 1 }}>
+        <Grid container spacing={2} direction="row">
+          <Grid item container spacing={2} direction="column">
+            <Grid item xs={12} sm={6} md={6} direction="column">
+              <Paper elevation={0} >
+                <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} direction="row">
+              <Paper>
+                <Typography>me</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} direction="row" >
+              <Paper>
+                <Typography>me</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} direction="row">
+              <Paper elevation={0} >
+                <Typography>me</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
+              </Paper>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
