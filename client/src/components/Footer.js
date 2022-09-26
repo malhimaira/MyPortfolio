99896b6df775df683fad58188cf13fdc582aac11
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-09-26 10:52:36 -0400

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Typography, Paper } from '@mui/material';
 function Footer() {
     return (
         <Paper elevation={0}>
-            <Typography sx={{ display: "flex", justifyContent: "center" }}>MAIRA MALHI 2022 ©️</Typography>
+            <Typography sx={{ display: "flex", justifyContent: "center" }}>MAIRA MALHI · COPYRIGHT 2022 ©</Typography>
         </Paper>
     )
 }
