b87c24d344a1d2a3139eaf0b8e1c9ff747d342f4
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2024-01-16 22:44:28 -0500

    create initial popup

--- client/src/pages/Paint.js
+++ client/src/pages/Paint.js
@@ -1,8 +1,14 @@
-import { Paper, Typography, Grid, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Paper, Typography, Grid, Box, Modal } from '@mui/material';
 import NavBar from '../components/NavBar.js'
 import Footer from '../components/Footer.js'
 
 function Paint() {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div>
       <Box sx={{ flexGrow: 1, padding: 1 }}>
@@ -12,9 +18,22 @@ function Paint() {
             <Paper elevation={0}>
              <Typography>PAINT</Typography>
             </Paper>
-            <Paper elevation={0}>
+            <Paper elevation={0} onClick={handleOpen}>
               <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
             </Paper>
+            <Modal
+              open={open}
+              onClose={handleClose}
+            >
+              <Paper>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description">
+                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </Typography>
+              </Paper>
+          </Modal>
             <Paper elevation={0}>
               <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
             </Paper>
