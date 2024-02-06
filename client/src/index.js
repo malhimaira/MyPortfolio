9a22458ca9a5bd3ff5da9b6ec976812013bc036a
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2024-02-05 22:30:30 -0500

    Corrected page to use single Modal updated with use state

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider  theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- client/src/pages/Paint.js
+++ client/src/pages/Paint.js
@@ -1,71 +1,95 @@
-import React, { useState } from 'react';
-import { Paper, Typography, Grid, Box, Modal } from '@mui/material';
-import NavBar from '../components/NavBar.js'
-import Footer from '../components/Footer.js'
+import React, { useState } from "react";
+import { Paper, Typography, Grid, Box, Modal } from "@mui/material";
+import NavBar from "../components/NavBar.js";
+import Footer from "../components/Footer.js";
 
 function Paint() {
-
   // handle modal
+  const [modalTitle, setModalTitle] = React.useState(null);
+  const [modalContent, setModalContent] = React.useState(null);
   const [open, setOpen] = React.useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const handleModalContent = (x) => setModalContent(x);
+  const handleModalTitle = (x) => setModalTitle(x);
+
   //list of art components
-  const leftArtwork = [{"title":"Ltest1","art":"xx"},{"title":"Ltest2","art":"xx"},{"title":"Ltest3","art":"xx"}];
-  const rightArtwork = [{"title":"Rtest1","art":"xx"},{"title":"Rtest2","art":"xx"},{"title":"Rtest3","art":"xx"}];
+  const leftArtwork = [
+    { title: "Ltest1", art: "x1" },
+    { title: "Ltest2", art: "x2" },
+    { title: "Ltest3", art: "x3" },
+  ];
+  const rightArtwork = [
+    { title: "Rtest1", art: "x4" },
+    { title: "Rtest2", art: "x5" },
+    { title: "Rtest3", art: "x6" },
+  ];
 
   return (
     <div>
       <Box sx={{ flexGrow: 1, padding: 1 }}>
         <Grid container spacing={2} direction="row">
-          <Grid item xs={12} sm={6} md={5} >
-            <NavBar/>
+          <Grid item xs={12} sm={6} md={5}>
+            <NavBar />
             <Paper elevation={0}>
-             <Typography>PAINT</Typography>
+              <Typography>PAINT</Typography>
             </Paper>
-            {leftArtwork.map(function(art, idL){
+            <Modal open={open} onClose={handleClose}>
+              <Paper>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  {modalContent}
+                </Typography>
+                <Typography id="modal-modal-description" variant="h2">
+                  {modalTitle}
+                </Typography>
+              </Paper>
+            </Modal>
+            {leftArtwork.map(function (art, idL) {
               return (
-              <div>
-                <Paper key={idL} elevation={0} onClick={handleOpen}>
-                  <Typography sx={{fontSize:'8rem'}}>{art.title}</Typography>  
-                  </Paper>
-                <Modal open={open} onClose={handleClose}>
-                  <Paper>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                      {art.art}
-                    </Typography>
-                    <Typography id="modal-modal-description" variant="h2" >
+                <div key={idL}>
+                  <Paper
+                    elevation={0}
+                    onClick={() => {
+                      handleOpen();
+                      handleModalContent(art.art);
+                      handleModalTitle(art.title);
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "8rem" }}>
                       {art.title}
                     </Typography>
                   </Paper>
-                </Modal>
-              </div>)
+                </div>
+              );
             })}
           </Grid>
           <Grid item xs={12} sm={6} md={7}>
-            {rightArtwork.map(function(art, idR){
-                return (
+            {rightArtwork.map(function (art, idR) {
+              return (
                 <div>
-                  <Paper key={idR} elevation={0} onClick={handleOpen}>
-                    <Typography sx={{fontSize:'8rem'}}>{art.title}</Typography>  
-                    </Paper>
-                  <Modal open={open} onClose={handleClose}>
-                    <Paper>
-                      <Typography id="modal-modal-title" variant="h6" component="h2">
-                        {art.art}
-                      </Typography>
-                      <Typography id="modal-modal-description" variant="h2" >
-                        {art.title}
-                      </Typography>
-                    </Paper>
-                  </Modal>
-                </div>)
-              })}
-            <Footer/>
+                  <Paper
+                    key={idR}
+                    elevation={0}
+                    onClick={() => {
+                      handleOpen();
+                      handleModalContent(art.art);
+                      handleModalTitle(art.title);
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "8rem" }}>
+                      {art.title}
+                    </Typography>
+                  </Paper>
+                </div>
+              );
+            })}
+            <Footer />
           </Grid>
         </Grid>
       </Box>
     </div>
-  )
+  );
 }
 export default Paint;
