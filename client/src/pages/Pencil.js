e8eb4e1c92af5ab1dfe002667c344076d4d13b30
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2024-04-01 09:47:38 -0400

    Update Pencil.js

--- client/src/pages/Pencil.js
+++ client/src/pages/Pencil.js
@@ -1,39 +1,94 @@
-import { Paper, Typography, Grid, Box } from '@mui/material';
-import NavBar from '../components/NavBar.js'
-import Footer from '../components/Footer.js'
+import React, { useState } from "react";
+import { Paper, Typography, Grid, Box, Modal } from "@mui/material";
+import NavBar from "../components/NavBar.js";
+import Footer from "../components/Footer.js";
 
 function Pencil() {
+  // handle modal
+  const [modalTitle, setModalTitle] = useState(null);
+  const [modalContent, setModalContent] = useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleModalContent = (x) => setModalContent(x);
+  const handleModalTitle = (x) => setModalTitle(x);
+
+  //list of art components
+  const leftArtwork = [
+    { title: "Ltest1", art: "x1" },
+    { title: "Ltest2", art: "x2" },
+    { title: "Ltest3", art: "x3" },
+  ];
+  const rightArtwork = [
+    { title: "Rtest1", art: "x4" },
+    { title: "Rtest2", art: "x5" },
+    { title: "Rtest3", art: "x6" },
+  ];
+
   return (
     <div>
       <Box sx={{ flexGrow: 1, padding: 1 }}>
         <Grid container spacing={2} direction="row">
-          <Grid item xs={12} sm={6} md={5} >
-            <NavBar/>
+          <Grid item xs={12} sm={6} md={5}>
+            <NavBar />
             <Paper elevation={0}>
-             <Typography>PENCIL</Typography>
+              <Typography>PENCIL</Typography>
             </Paper>
-            <Paper elevation={0}>
-              <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-            <Paper elevation={0}>
-              <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
+            <Modal open={open} onClose={handleClose}>
+              <Paper elevation={0}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  {modalContent}
+                </Typography>
+                <Typography id="modal-modal-description" variant="h2">
+                  {modalTitle}
+                </Typography>
+              </Paper>
+            </Modal>
+            {leftArtwork.map((art) => {
+              return (
+                <div key={art.title}>
+                  <Paper
+                    elevation={0}
+                    onClick={() => {
+                      handleOpen();
+                      handleModalContent(art.art);
+                      handleModalTitle(art.title);
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "4rem" }}>
+                      {art.title}
+                    </Typography>
+                  </Paper>
+                </div>
+              );
+            })}
           </Grid>
           <Grid item xs={12} sm={6} md={7}>
-            <Paper elevation={0}>
-              <Typography>BBB</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-            <Paper elevation={0}>
-              <Typography>BBB</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-            <Paper elevation={0}>
-              <Typography>BBB</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
-            </Paper>
-            <Footer/>
+            {rightArtwork.map((art) => {
+              return (
+                <div key={art.title}>
+                  <Paper
+                    elevation={0}
+                    onClick={() => {
+                      handleOpen();
+                      handleModalContent(art.art);
+                      handleModalTitle(art.title);
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "4rem" }}>
+                      {art.title}
+                    </Typography>
+                  </Paper>
+                </div>
+              );
+            })}
+            <Footer />
           </Grid>
         </Grid>
       </Box>
     </div>
-  )
+  );
 }
 export default Pencil;
