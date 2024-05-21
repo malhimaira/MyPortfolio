54ac3256fe18953ddf30782c94e70c330abdbd54
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2024-05-20 22:24:57 -0400

    small fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
     <div>
       <Box sx={{ flexGrow: 1, padding: 1 }}>
         <Grid container spacing={2} direction="row">
-          <Grid item xs={12} sm={5} md={5} >
+          <Grid item xs={12} sm={7} md={7} >
             <NavBar />
             <Paper elevation={0}>
               <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
@@ -30,11 +30,11 @@ function Home() {
               <Typography>AAAA</Typography>  <Typography>allo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
             </Paper>
           </Grid>
-          <Grid item xs={12} sm={7} md={7}>
+          <Grid item xs={12} sm={5} md={5}>
             <Boat> 
               <Typography variant='h3' > ART <br/> IS <br/> FOR- <br/> EVER. </Typography>
               <br/>
-              <Typography variant='h2' sx={{color:'white', fontStyle:'italic'}} > Thomas Kinkade </Typography>
+              <Typography variant='h2' sx={{color:'white', fontStyle:'italic', fontSize:'4.2vw'}} > Thomas Kinkade </Typography>
             </Boat>
             <Paper elevation={0}>
               <Typography>BBB</Typography>  <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>me</Typography>   <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>  <Typography>Helo</Typography>
